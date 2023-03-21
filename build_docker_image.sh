6a72e88f7df290d3e5f16f606a9ef67c9e3f5c3e
Fred Bitenyo <fredbitenyo@gmail.com>
2023-03-21 21:25:15 +0000

    final correction for codepipeline

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,39 +1,17 @@
 #!/bin/bash
+
 # Author: Fred Bitenyo
 
 # create a repository to store the docker image in docker hub
 
 # install and configure docker on the ec2 instance
-if ! docker --version &> /dev/null
-then
-  if hash yum; then
-    echo "Amazon Linux detected"
-    echo "installing docker in amazon linux"
-    sudo yum update -y
-    sudo amazon-linux-extras install docker -y
-    sudo service docker start
-    sudo systemctl enable docker
-  elif hash lsb_release; then
-    echo "installing docker in Ubuntu"
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt install docker.io
-    systemctl start docker
-    systemctl enable docker
-    docker --version
-  else
-   echo "Unsupported operating system"
-   exit 1
-  fi
-fi
 
-# test python code
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ubuntu/out.log 2>&1
+# exec 3>&1 4>&2
+# trap 'exec 2>&4 1>&3' 0 1 2 3
+# exec 1>/home/ubuntu/out.log 2>&1
 # build the docker image
 echo "building a docker image"
-sudo docker build -t streamlit:1.0 .
+docker build -t streamlit:1.0 .
 
 # login to your docker hub account
 # cat ~/my_password.txt | sudo docker login --username hirodaridevdock --password-stdin
@@ -46,4 +24,4 @@ sudo docker build -t streamlit:1.0 .
 
 # start the container to test the image
 echo "running docker run"
-sudo docker run -dp 8501:8501 streamlit:1.0
+docker run -dp 8501:8501 streamlit:1.0
--- install_docker.sh
+++ install_docker.sh
@@ -1,16 +1,66 @@
-# install and configure docker on the ec2 instance
-if ! docker --version &> /dev/null
-then
+#!/bin/bash -xe
+
+
+
+
+# exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+# AUTOUPDATE=false
+
+
+function platformize(){
+
+#Linux OS detection#
+ if hash lsb_release; then
+   echo "Ubuntu server OS detected"
+   export PLAT="ubuntu"
+
+
+elif hash yum; then
+  echo "Amazon Linux detected"
+  export PLAT="amz"
+
+ else
+   echo "Unsupported release"
+   exit 1
+
+ fi
+}
+
+
+function execute(){
+
+if [ ${PLAT} = "ubuntu" ]; then
+
+  # echo "installing docker ubuntu"
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  sudo apt update -y
+  sudo apt-cache policy docker-ce
+  sudo apt install docker-ce -y
+# Linux post-install
+  sudo usermod -aG docker $USER
+  sudo systemctl enable docker
+  
+
+elif [ ${PLAT} = "amz" ]; then
+
   echo "installing docker amazon-linux"
-  # sudo yum update -y
-  # sudo amazon-linux-extras install docker -y
-  # sudo service docker start
-  # sudo systemctl enable docker
-  echo "installing docker ubuntu"
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt install docker.io -y
-  systemctl start docker
-  systemctl enable docker
-  docker --version
+  sudo yum update -y
+  sudo amazon-linux-extras install docker -y
+  echo "making sure the permission is granted"
+  sudo usermod -aG docker ${USER}
+  sudo systemctl enable docker.service  
+  sudo systemctl start docker.service
+  echo "running hello-world docker"
+  sudo docker run hello-world
+
+else
+  echo "Unsupported platform ''${PLAT}''"
 fi
+
+}
+
+platformize
+execute
+
